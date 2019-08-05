9577ce8eba70e6dbc8c7f7f7502ab7f4c54117ae
boonleng <boonleng@ou.edu>
2019-08-05 09:41:39 -0500

    Organization changed. More on foundation.py

--- foundation.py
+++ foundation.py
@@ -1,16 +1,30 @@
 import os
+import glob
+import time
 import json
 import logging
+import requests
+import datetime
 
+# Import the token from a secure location
+os.sys.path.append(os.path.expanduser('~/.ssh'))
+import tesla
+
+# Some global constants
 logger = logging.getLogger('Tesla')
+dataLogHome = os.path.expanduser('~/Documents/Tesla')
 
-def showMessageLevel(level):
-    logger.setLevel(level)
+def showMessageLevel(level=logging.WARNING):
     if len(logger.handlers) == 0:
         handler = logging.StreamHandler()
-        handler.setLevel(level)
         handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S'))
         logger.addHandler(handler)
+    else:
+        handler = logger.landlers[0]
+        handler.setLevel(level)
+
+def showWarningMessages():
+    showMessageLevel(logging.WARNING)
 
 def showDebugMessages():
     showMessageLevel(logging.DEBUG)
@@ -18,12 +32,230 @@ def showDebugMessages():
 def showInfoMessages():
     showMessageLevel(logging.INFO)
 
+# Data log folder
+folders = ['~/logs', '{}/logs'.format(dataLogHome)]
+logfile = None
+for folder in folders:
+    folder = os.path.expanduser(folder)
+    if os.path.exists(folder):
+        logfile = '{}/tesla-{}.log'.format(folder, time.strftime('%Y%m%d', time.localtime(time.time())))
+if logfile is None:
+    logfile = 'messages.log'
+logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+logging.Formatter.converter = time.gmtime
+
+logger.setLevel(logging.INFO)
+
 def shortenPath(path, n):
     parts = path.split('/')
     return '...{}'.format('/'.join(parts[-n:])) if len(parts) > n else path
+
+def requestData():
+    url = 'https://owner-api.teslamotors.com/api/1/vehicles/{}/vehicle_data'.format(tesla.vehicle_id)
+    headers = {
+        'Authorization': 'Bearer {}'.format(tesla.token)
+    }
+    res = requests.get(url, headers=headers)
+    dat = res.json()['response']
+    return dat
 
 def objFromFile(filename):
     with open(filename, 'r') as fobj:
         json_str = fobj.readline()
         fobj.close()
     return json.loads(json_str)
+
+def getLatestDays(count=31):
+    folders = glob.glob('{}/2*'.format(dataLogHome))
+    folders.sort()
+
+    t = []
+    d = []
+    for folder in folders[-count:]:
+        files = glob.glob('{}/*.json'.format(folder))
+        files.sort()
+        dd = []
+        for file in files:
+            #print('{}'.format(file))
+            data = objFromFile(file)
+            dd.append(data)
+        d.append(dd)
+        o = d[-1][-1]['charge_state']
+        t.append(datetime.datetime.fromtimestamp(o['timestamp'] / 1000))
+        # print('{} -> {}% {} {}'.format(os.path.basename(file), o['battery_level'], o['charging_state'], t[-1].weekday()))
+    return t, d
+
+def getCalendarArray():
+    # Get the data from the latest 31 days
+    t, d = getLatestDays()
+
+    # Find the 4th Sunday before present day
+    t0 = t[-1]
+    oneday = datetime.timedelta(days=1)
+    while t0.weekday() != 6:
+        t0 -= oneday
+    t0 -= datetime.timedelta(days=21)
+    # display('{}'.format(t0.strftime('%Y/%m/%d %H:%M %A')))
+
+    # The first Sunday
+    t0 = datetime.datetime.strptime(t0.strftime('%Y-%m-%d'), '%Y-%m-%d')
+    # display('{}'.format(t0.strftime('%Y/%m/%d %H:%M %A')))
+
+    # Group the weeks
+    tt = []
+    dd = []
+    k = 0
+
+    # Make a 4-week array of data arrays
+    for i in range(4):
+        tw = []
+        dw = []
+        for i in range(7):
+            if k < len(t) and t0 <= t[k] and t[k] < (t0 + oneday):
+                tw.append(t[k])
+                dw.append(d[k])
+                k += 1
+            else:
+                tw.append(t0)
+                dw.append(None)
+            t0 += oneday
+        tt.append(tw)
+        dd.append(dw)
+
+    return tt, dd
+
+def getDataInHTML(figsize=(900, 640), o=0.04):
+
+    tt, dd = getCalendarArray()
+
+    # Size in pixels
+    w = figsize[0] / (7 + 6 * o)
+    h = figsize[1] / (4.5 + 3 * o)
+
+    # Offset in pixels
+    o = w * o
+
+    code = '<html>'
+    code += '<head>'
+    code += '<meta charset="UTF-8">'
+    code += '<style type="text/css">'
+    code += '@font-face {{font-family:"Helvetica Neue"; src:url(fonts/HelveticaNeueLight.ttf)}} '
+    code += 'html {{font-family: "Helvetica Neue", Arial, sans-serif; font-size:10pt}} '
+    code += '.box-container {{display:block; position:relative; top:10px; margin:0 auto; width:{}px; height:{}px; background-color:white}} '.format(figsize[0], figsize[1])
+
+    code += '.box {{display:block; position:absolute; width:{}px; height:{}px; border:solid 1px #aaa}}'.format(w, h)
+    code += '.title {{display:block; position:absolute; top:0}} '
+    code += '.titleMonth, .titleYear {{display:inline-block; font-size:2.0em;}} '
+    code += '.titleMonth {{font-weight:500;}} '
+    code += '.dayOfWeek {{display:block; position:absolute; width:{}px; height:{}px; text-align:right}} '.format(w, 0.2 * h)
+
+    code += '.dayLabel, .titleDayLabel {{display:block; position:absolute; top:5px; right:5px; font-size:1.1em; z-index:100}} '
+    code += '.titleDayLabel {{font-size:1.2em; }} '
+    code += '.otherMonth {{color:#aaa;}} '
+    code += '.batteryLevel {{display:block; position:absolute; bottom:0; width:100%; background-color:#99ff00; z-index:0}} '
+    code += '.lowCharge {{background-color:#ffcc00}} '
+
+    code += '.info {{display:block; position:absolute; bottom:10%; width:95%; margin:0 2.5%; padding:0; text-align:center}} '
+    code += '.textInfo {{display:block; width:100%}} '
+    code += '.large {{font-size:1.8em; font-weight:500; font-stretch:extra-expanded; margin-bottom:0.3em}} '
+    code += '.medium {{line-height:1.2em}} '
+    code += '.tiny {{line-height:0.8em}} '
+
+    code += '.iconBar {{display:block; position:absolute; top:8px; left:8px; width:100%}}'
+    code += 'img.icon {{float:left; margin:0; width:18px; padding:2px}} '
+    code += '</style>'
+    code += '</head>'
+
+    code += '<body>'
+
+    code += '<div class="box-container">'
+
+    code += '<div class="title">'
+    code += '<span class="titleMonth">{}</span>&nbsp; <span class="titleYear">{}</span>'.format(tt[-1][0].strftime('%B'), tt[0][0].strftime('%Y'))
+    code += ' <span class="titleMonth">&nbsp; 🚘 🔋 ⚡️</span>'
+    code += '</div>'
+
+    # Use the latest day to decide the target month
+    targetMonth = tt[-1][0].month
+
+    # The top row show days of the week
+    for i in range(7):
+        x = i * (w + o)
+        y = 0.28 * h
+        code += '<div class="dayOfWeek" style="left:{:.2f}px; top:{:.2f}px">'.format(x, y,)
+        code += '<span class="titleDayLabel">{}</span>'.format(tt[0][i].strftime('%a'))
+        code += '</div>'
+
+    # Odometer reading from the previous day
+    o1 = 0.0
+
+    # Now we go through the days
+    mo = tt[0][0].month
+    for j in range(len(tt)):
+        for i in range(len(tt[j])):
+            x = i * (w + o)
+            y = j * (h + o) + 0.5 * h
+            xc = x + 0.5 * w
+
+            # Background
+            code += '<div class="box" style="left:{:.2f}px; top:{:.2f}px">'.format(x, y)
+
+            # The day label.
+            if mo != tt[j][i].month:
+                mo = tt[j][i].month
+                dayString = '{}'.format(tt[j][i].strftime('%b %-d'))
+            else:
+                dayString = '{}'.format(tt[j][i].strftime('%-d'))
+            if targetMonth != mo:
+                elementClass = ' otherMonth'
+            else:
+                elementClass = ''
+            code += '<span class="dayLabel{}">{}</span>'.format(elementClass, dayString)
+
+            # Skip the day if there is no data
+            if dd[j][i] is None:
+                code += '</div>'
+                continue
+
+            # Battery level
+            dayArray = dd[j][i]
+            batteryLevel = dayArray[-1]['charge_state']['battery_level']
+            if batteryLevel <= 60.0:
+                elementClass = ' lowCharge'
+            else:
+                elementClass = ''
+            code += '<div class="batteryLevel{}" style="height:{}%"></div>'.format(elementClass, batteryLevel)
+
+            # Calculate total miles driven
+            if o1 == 0.0:
+                o1 = dayArray[0]['vehicle_state']['odometer']
+            o0 = dayArray[-1]['vehicle_state']['odometer']
+            delta_o = o0 - o1
+            o1 = o0
+
+            #
+            sw = dayArray[-1]['vehicle_state']['car_version']
+
+            # Icon bar using the information derived earlier
+            code += '<div class="iconBar">'
+            if any([d['charge_state']['charging_state'] == 'Charging' for d in dayArray]):
+                code += '<img class="icon" src="blob/charge-0.png">'
+            if delta_o > 20:
+                code += '<img class="icon" src="blob/wheel.png">'
+            code += '</div>'
+
+            # Lines of information
+            code += '<div class="info">'
+            code += '<span class="textInfo large">{}%</span>'.format(batteryLevel)
+            code += '<span class="textInfo medium">{}</span>'.format(tt[j][i].strftime('%I:%M %p'))
+            code += '<span class="textInfo medium">{:.1f} mi ({})</span>'.format(delta_o, len(dayArray))
+            code += '<span class="textInfo small">{}</span>'.format(sw)
+            code += '</div>'
+
+            code += '</div>'
+
+    code += '</div>'
+    code += '</body>'
+    code += '</html>'
+
+    return code
--- getStat.py
+++ getStat.py
@@ -8,6 +8,9 @@
 
     Updates
 
+    1.1    - 8/5/2019
+           - Moved a lot of reusable functions to foundation.py
+
     1.0    - 8/2/2019
            - It is working. We can schedule this to run routinely through a cronjob
 
@@ -16,47 +19,14 @@
 
 """
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 import os
 import time
 import argparse
-import logging
-import requests
-import json
 
-dataLogHome = os.path.expanduser('~/Documents/Tesla/')
+import foundation
 
-logger = logging.getLogger('Tesla')
-folders = ['~/logs', '{}/logs'.format(dataLogHome)]
-logfile = None
-for folder in folders:
-    folder = os.path.expanduser(folder)
-    if os.path.exists(folder):
-        logfile = '{}/tesla-{}.log'.format(folder, time.strftime('%Y%m%d', time.localtime(time.time())))
-if logfile is None:
-    logfile = 'messages.log'
-logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
-logging.Formatter.converter = time.gmtime
-
-os.sys.path.append(os.path.expanduser('~/.ssh'))
-
-import tesla
-
-def requestData():
-    url = 'https://owner-api.teslamotors.com/api/1/vehicles/{}/vehicle_data'.format(tesla.vehicle_id)
-    headers = {
-        'Authorization': 'Bearer {}'.format(tesla.token)
-    }
-    res = requests.get(url, headers=headers)
-    dat = res.json()['response']
-    return dat
-
-def objFromFile(filename):
-    with open(filename, 'r') as fobj:
-        json_str = fobj.readline()
-        fobj.close()
-    return json.loads(json_str)
 
 #
 #     M  A  I  N
@@ -77,31 +47,24 @@ if __name__ == '__main__':
     parser.add_argument('-v', default=0, action='count', help='increase the verbosity level')
     args = parser.parse_args()
 
-    # Another stream handler to show output on screen
-    handler = logging.StreamHandler()
+    # Set the logger to output something if verbosity flag is set
     if args.v > 1:
-        handler.setLevel(logging.DEBUG)
-        logger.setLevel(logging.DEBUG)
+        foundation.showDebugMessages()
     elif args.v:
-        handler.setLevel(logging.INFO)
-    else:
-        handler.setLevel(logging.WARNING)
-    handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S'))
-    logger.addHandler(handler)
+        foundation.showInfoMessages()
 
     # Log an entry
-    logger.debug('--- Started ----------')
-    logger.info('Tesla Data Logger {}'.format(__version__))
+    foundation.logger.debug('--- Started ----------')
+    foundation.logger.info('Tesla Data Logger {}'.format(__version__))
 
-    dat = requestData()
+    dat = foundation.requestData()
 
     if dat is None:
-        if args.v:
-            logger.info('Vehicle is sleeping')
+        foundation.logger.info('Vehicle is sleeping')
     else:
         jsonString = json.dumps(dat)
 
-        logger.info('Data received. battery_level = {}   range = {} / {}   write = {}'.format(
+        foundation.logger.info('Data received. battery_level = {}   range = {} / {}   write = {}'.format(
             dat['charge_state']['battery_level'],
             dat['charge_state']['battery_range'],
             dat['charge_state']['ideal_battery_range'],
