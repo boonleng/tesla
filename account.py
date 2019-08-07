ab6142d824a3c1c3e0a87dc80ca3627c42654a0b
boonleng <boonleng@ou.edu>
2019-08-06 19:10:18 -0500

    Username and access token are stored properly

--- account.py
+++ account.py
@@ -1,6 +1,8 @@
 import os
+import json
 import keyring
 import getpass
+import requests
 import configparser
 
 import foundation
@@ -13,28 +15,54 @@ def _get_rc():
         foundation.logger.info('No configuration file. Setting up ...')
         try:
             username = input('Enter username: ')
+            if len(username) == 0:
+                foundation.logger.info('Setup aborted. No username provided.')
+                return None
         except KeyboardInterrupt:
-            foundation.logger.exception('Setup aborted.')
+            foundation.logger.exception('Setup aborted. User aborted the setup.')
             return None
 
-        config = configparser.ConfigParser()
-        config.add_section('user')
-        config['user'] = {'username': username}
-
-        with open(RC_FILE, 'w') as fid:
-            config.write(fid)
-
-        if not keyring.get_password(SITE, username):
+        password = keyring.get_password(SITE, username)
+        if not password:
             password = getpass.getpass('Enter password: ')
             if not password:
-                foundation.logger.info('User did not enter a password.')
+                foundation.logger.info('Setup aborted. No password provided.')
                 return None
             keyring.set_password(SITE, username, password)
             foundation.logger.info('Password saved to Keychain Access')
 
+        # Retrieve a token using the login provided
+        url = 'https://{}/oauto/token'.format(SITE)
+        payload = {
+            'grant_type': 'password',
+            'client_id': '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
+            'client_secret': 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3',
+            'email': username,
+            'password': password
+        }
+        headers = {
+            'Host': SITE,
+            'User-Agent': 'Learning',
+            'Content-Type': 'application/json'
+        }
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+        if r.status_code != 200:
+            foundation.logger.exception('Unable to retrieve token.')
+            return None
+        token = r.json()
+
+        # Save username and token into the configuration
+        config = configparser.ConfigParser()
+        config.add_section('user')
+        config.add_section('token')
+        config['user'] = {'username': username}
+        config['token'] = token
+        with open(RC_FILE, 'w') as fid:
+            config.write(fid)
+
         foundation.logger.info('Config setup complete')
 
-
+    # Make a config and load the configuration
     config = configparser.ConfigParser()
     try:
         config.read(RC_FILE)
@@ -42,12 +70,14 @@ def _get_rc():
         foundation.logger.exception('Bad config file.')
         raise ConfigError from e
 
-    if 'user' not in config.sections():
+    # Assess the completeness
+    if not all(sec in config.sections() for sec in ['user', 'token']):
+        foundation.logger.exception('Bad config file. Try removing it and rerun this script.')
         return None
-    config = dict(config['user'])
-    return config
+    return {'username':config['user']['username'], 'token':dict(config['token'])}
 
 config = _get_rc();
 
 # print(config['username'])
+# print(config['token'])
 # print(keyring.get_password(SITE, config['username']))
